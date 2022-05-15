60ba044057ece3667e2631b1ebc298c669f3738e
SnehaR03 <105554370+SnehaR03@users.noreply.github.com>
2022-05-15 18:43:36 +0530

    Update PrintDuplicatesInArray.java

--- week1/day2/classroom/arrays/PrintDuplicatesInArray.java
+++ week1/day2/classroom/arrays/PrintDuplicatesInArray.java
@@ -5,6 +5,25 @@ public class PrintDuplicatesInArray {
 	public static void main(String[] args) {
 		
 		int[] arr = {14,12,13,11,15,14,18,16,17,19,18,17,20};
+		int length=arr.length;
+		int count;
+		for(int i=0;i<length-1;i++) {
+			count=0;
+			for(int j=i+1;j<length;j++) {
+				if(arr[i]==arr[j]) {
+					count=count+1;
+					
+					//System.out.println("Duplicate value:"+arr[j]);
+				}
+				
+			}
+			if(count>0)
+			{
+				System.out.println("Duplicate value:"+arr[i]);
+			}
+			
+			
+		}
 		
 		// get the length of the array
 		// declare an int variable named count
@@ -24,5 +43,7 @@ public class PrintDuplicatesInArray {
 			
 		
 	}
+
+
 
 }
